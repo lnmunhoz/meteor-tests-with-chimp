f0665d525f1d85a2e453f9ee9816b24822c16d9c
lnmunhoz <ln.munhoz@gmail.com>
2016-10-24 00:55:25 -0200

    Add validation to publication

--- imports/api/books/server/publications.js
+++ imports/api/books/server/publications.js
@@ -1,6 +1,13 @@
 import { Meteor } from 'meteor/meteor';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
 import '../books';
 
-Meteor.publish('books', () => {
-  return Books.find();
+Meteor.publish('books', ({ skip = 0, limit = 100 }) => {
+  new SimpleSchema({
+    skip: { type: Number },
+    limit: { type: Number },
+  }).validate({ skip, limit });
+
+  return Books.find({}, { skip, limit });
 });
